5cc31cebc8ade7e652267ee3b5888b6ee18a98c1
roo-ham <roohamcom@gmail.com>
2023-11-06 19:19:04 +0900

    Hello, Hanyang!

--- src/__main__.py
+++ src/__main__.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import rospy
+from cv_bridge import CvBridge
+import numpy, cv2
+import os
+'''
+from sensor_msgs.msg import Image
 from ar_track_alvar_msgs.msg import AlvarMarkers
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+'''
 from basement import Basement
 
 # 클래스 생성
 class Main:
     def __init__(self):
-        print("hi")
+        rospy.init_node("hyproject_node")
+        #self.bridge = CvBridge()
+        #rospy.Subscriber("/camera/rgb/image_raw", Image, self.camera_CB)
+        #rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.marker_CB)
+        #rospy.Subscriber('/scan', LaserScan, self.laser_callback)
+        #self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        #self.drive_data = Twist()
+        #self.basic_speed = 0.3
+        os.system("clear")
+        print("Hello, Hanyang!")
+        print("Ctrl+C to exit.")
 
 
 if __name__ == "__main__":
-    # 클래스 객체 생성
     Main()
     try:
-        # 함수를 호출하여 ROS 노드를 실행하고, 메시지를 수신하는 동안 무한 루프
-        rospy.spin()
-    # 예외가 발생하면, 예외를 무시하고 프로그램 종료
+        while not rospy.is_shutdown():
+            pass
     except rospy.ROSInterruptException:
         pass
