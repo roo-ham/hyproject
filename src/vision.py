6ecd5cac9f521790d91369a7eb49ef85ac9aff4d
roo-ham <roohamcom@gmail.com>
2023-11-07 16:28:17 +0900

    bugfixed finally

--- src/vision.py
+++ src/vision.py
@@ -1,30 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import rospy
 from cv_bridge import CvBridge
 import numpy, cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
 from basement import Basement
 
 class VisionImage:
     def __init__(self):
-        self.bridge:CvBridge = CvBridge()
-        rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+        self.bridge = CvBridge()
+        rospy.Subscriber("/camera/rgb/image_raw/compressed", CompressedImage, self.callback)
         self.img = None
         print("I'm VisionImage")
     def callback(self, data):
-        print("VisionImage : I've got callback")
-        self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.img = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
         self.img = cv2.resize(self.img, (128, 128))
         cv2.namedWindow("hyproject", cv2.WINDOW_NORMAL)
         cv2.imshow("hyproject", self.img)
+        cv2.waitKey(1)
 
 class VisionMarker:
     def __init__(self):
         print("I'm VisionMarker")
         rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.callback)
     def callback(self, data):
-        print("VisionMarker : I've got callback")
+        pass
 
 
 if __name__ == "__main__":
