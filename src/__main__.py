dcc6d03c59de6cca1fbf2a55b61dbcabeb146110
RooHam <roohamcomp@gmail.com>
2023-11-07 15:07:35 +0900

    vision 분리

--- src/__main__.py
+++ src/__main__.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import rospy
-from cv_bridge import CvBridge
-import numpy, cv2
 import os
 '''
-from sensor_msgs.msg import Image
-from ar_track_alvar_msgs.msg import AlvarMarkers
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 '''
+
+from vision import *
 from basement import Basement
 
 # 클래스 생성
 class Main:
     def __init__(self):
         rospy.init_node("hyproject_node")
-        #self.bridge = CvBridge()
-        #rospy.Subscriber("/camera/rgb/image_raw", Image, self.camera_CB)
-        #rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.marker_CB)
+        self.vision_image = VisionImage()
+        self.vision_marker = VisionMarker()
         #rospy.Subscriber('/scan', LaserScan, self.laser_callback)
         #self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         #self.drive_data = Twist()
-        #self.basic_speed = 0.3
-        os.system("clear")
-        print("Hello, Hanyang!")
-        print("Ctrl+C to exit.")
 
 
 if __name__ == "__main__":
     Main()
     try:
         while not rospy.is_shutdown():
-            pass
+            os.system("clear")
+            print("Hello, Hanyang!")
+            print("Ctrl+C to exit.")
+            rospy
     except rospy.ROSInterruptException:
         pass
---
+++ src/vision.py
@@ -0,0 +1,35 @@
+import rospy
+from cv_bridge import CvBridge
+import numpy, cv2
+from sensor_msgs.msg import Image
+from ar_track_alvar_msgs.msg import AlvarMarkers
+
+from basement import Basementd
+
+class VisionImage:
+    def __init__(self):
+        self.bridge = CvBridge()
+        rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+        self.img = None
+        cv2.namedWindow("hyproject", cv2.WINDOW_NORMAL)
+    def callback(self, data):
+        self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.img = cv2.resize(self.img, (128, 128))
+        cv2.imshow("hyproject", self.image)
+        cv2.waitKey(1)
+
+class VisionMarker:
+    def __init__(self):
+        rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.callback)
+    def callback(self):
+        pass
+
+
+if __name__ == "__main__":
+    VisionImage()
+    VisionMarker()
+    try:
+        while not rospy.is_shutdown():
+            pass
+    except rospy.ROSInterruptException:
+        pass
