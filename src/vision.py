13e7531690c639dff1a51e99d6832ed890afade3
roo-ham <roohamcom@gmail.com>
2023-11-08 16:56:10 +0900

    asfd

--- src/vision.py
+++ src/vision.py
@@ -13,19 +13,21 @@ class VisionImage:
     def __init__(self):
         self.bridge = CvBridge()
         rospy.Subscriber("/camera/rgb/image_raw/compressed", CompressedImage, self.callback)
-        self.img = None
+        self.origin_top = np.zeros((128,256,3))
+        self.origin_bottom = np.zeros((128,256,3))
         print("I'm VisionImage")
     def callback(self, data):
-        self.img = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
-        self.img = cv2.resize(self.img, (256, 256), interpolation=cv2.INTER_NEAREST)
-        self.img = self.img[128:256, :, :]
-        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
-        self.img[:, :, 1:3] = 255
-        under_yellow = self.img[:, :, 0] < 15
-        over_yellow = self.img[:, :, 0] > 35
+        origin = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+        origin = cv2.resize(origin, (256, 256), interpolation=cv2.INTER_NEAREST)
+        self.origin_top = origin[128:256, :, :]
+        self.origin_bottom = origin[0:128, :, :]
+    def get_yellow(self):
+        img = cv2.cvtColor(self.origin_bottom, cv2.COLOR_BGR2HSV)
+        under_yellow = img[:, :, 0] < 15
+        over_yellow = img[:, :, 0] > 35
         yellow = over_yellow & under_yellow
-        self.img[:, :, 0] = np.where(under_yellow, 0, self.img[:, :, 0])
-        self.img[:, :, 1] = np.where(under_yellow, 50, 255)
+        img[:, :, 0] = np.where(under_yellow, 0, self.img[:, :, 0])
+        img[:, :, 1] = np.where(under_yellow, 50, 255)
         self.img[:, :, 0] = np.where(over_yellow, 128, self.img[:, :, 0])
         self.img[:, :, 2] = np.where(over_yellow, 50, 255)
         self.img = cv2.cvtColor(self.img, cv2.COLOR_HSV2BGR)
