87f35bff91386685d9fc47cffab81a01b8f5be61
RooHam <roohamcomp@gmail.com>
2023-11-09 12:56:56 +0900

    Update quick_start.py

--- quick_start.py
+++ quick_start.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-os.system("bash -c 'source ../../devel/setup.bash'")
-os.system("roslaunch launch/start.launch")
+os.system("bash -c 'source ../../devel/setup.bash && roslaunch launch/start.launch'")
+print("quick_start.py가 종료되었습니다.")
