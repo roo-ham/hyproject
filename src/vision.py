f2a2dd86b8eb204667cd35b4eae74d6d17f86673
RooHam <roohamcomp@gmail.com>
2023-11-12 01:13:37 +0900

    submodule class 추가

--- src/vision.py
+++ src/vision.py
@@ -4,15 +4,15 @@ import rospy
 import numpy as np
 import cv2
 from basement import Basement
+from submodule import Submodule
 from sensor_msgs.msg import CompressedImage
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from cv_bridge import CvBridge
 
-class VisionImage:
+class VisionImage(Submodule):
     def __init__(self, base:Basement):
-        self.basement = base
+        super().__init__(base, "VisionImage")
         self.sub_image_raw = rospy.Subscriber("/camera/rgb/image_raw/compressed", CompressedImage, self.callback)
-        self.timeout = 300
         print("I'm VisionImage")
     def get_yellow(self):
         under_yellow = self.basement.img_h < 15
@@ -27,7 +27,7 @@ class VisionImage:
         over_bri = self.basement.img_v < 50
         return (over_sat & over_bri)
     def update(self):
-        self.timeout -= 1
+        super().update()
         img = self.basement.get_bgr_bottom()
         yellow = self.get_yellow()
         white = self.get_white()
@@ -45,17 +45,13 @@ class VisionImage:
         cv2.imshow("hyproject", img)
         cv2.waitKey(1)
     def callback(self, data):
-        self.timeout = 60
+        super().callback(data)
         bridge = CvBridge()
         origin = bridge.compressed_imgmsg_to_cv2(data, "bgr8")
         origin = cv2.resize(origin, (256, 256), interpolation=cv2.INTER_NEAREST)
         self.basement.set_bgr(origin, origin[128:256, :, :])
 
-class VisionMarker:
+class VisionMarker(Submodule):
     def __init__(self, base:Basement):
-        self.basement = base
+        super().__init__(base, "VisionMarker")
         self.sub_marker = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.callback)
-        self.timeout = 300
-        print("I'm VisionMarker")
-    def callback(self, data):
-        pass
