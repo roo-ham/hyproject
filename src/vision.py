429a3bc0c35a437032bb01e914656eaa1bbf8eb5
RooHam <roohamcomp@gmail.com>
2023-11-12 01:32:04 +0900

    change color see

--- src/vision.py
+++ src/vision.py
@@ -31,15 +31,10 @@ class VisionImage(Submodule):
         yellow = self.get_yellow()
         white = self.get_white()
         black = self.get_black()
-        img[:, :, 0] = np.where(yellow, 20, img[:, :, 0])
-        img[:, :, 1] = np.where(yellow, 255, img[:, :, 1])
-        img[:, :, 2] = np.where(yellow, 255, img[:, :, 2])
-
-        img[:, :, 1] = np.where(white, 0, img[:, :, 1])
-        img[:, :, 2] = np.where(white, 255, img[:, :, 2])
-
-        img[:, :, 1] = np.where(black, 0, img[:, :, 1])
-        img[:, :, 2] = np.where(black, 0, img[:, :, 2])
+        img[:, :, 1] = 0
+        img[:, :] = np.where(yellow, (20, 255, 255), img[:, :])
+        img[:, :] = np.where(white, (128, 255, 255), img[:, :])
+        img[:, :] = np.where(black, (128, 255, 0), img[:, :])
         cv2.namedWindow("hyproject", cv2.WINDOW_NORMAL)
         cv2.imshow("hyproject", img)
         cv2.waitKey(1)
