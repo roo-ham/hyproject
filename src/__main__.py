b3e8e9b409bee1af07fb1709d33609c698b60278
RooHam <roohamcomp@gmail.com>
2023-11-12 00:32:56 +0900

    .

--- src/__main__.py
+++ src/__main__.py
@@ -17,7 +17,7 @@ class Main:
         self.basement = base
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
-        self.launch = roslaunch.parent.ROSLaunchParent(uuid, ["./camera.launch"])
+        self.launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/wego/1273_ws/src/hyproject/src/camera.launch"])
         self.launch.start()
         self.vision_image = VisionImage(base)
         self.vision_marker = VisionMarker(base)
