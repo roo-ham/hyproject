a0e9d33dab2fef979b48eba891a515ba4bf27581
RooHam <roohamcomp@gmail.com>
2023-11-12 15:03:30 +0900

    .

--- src/vision.py
+++ src/vision.py
@@ -33,9 +33,12 @@ class VisionImage(Submodule):
         return thick_v
     def get_yellow_hv(self):
         yellow = self.get_yellow()
+        white = self.get_white()
+        yellow &= ~white
         yellow_thick_h = self.get_thick_h(yellow)
         horizonal = np.zeros((128,256), bool) | yellow
         horizonal[0:127, :] &= ~yellow_thick_h[1:128, :]
+        horizonal[:, 0:252] &= horizonal[:, 4:256]
         yellow_thick_v = self.get_thick_v(horizonal)
         vertical = np.zeros((128,256), bool) | horizonal
         vertical[:, 0:255] &= ~yellow_thick_v[:, 1:256]
@@ -55,8 +58,9 @@ class VisionImage(Submodule):
         a, b = self.get_yellow_hv()
         #white = self.get_white()
         #black = self.get_black()
-        img[:, :, 0] = np.where(a, 255, img[:, :, 0])
-        img[:, :, 1] = np.where(a, 255, img[:, :, 1])
+        img[:, :, 0] = np.where(a&b, 255, img[:, :, 0])
+        img[:, :, 1] = np.where(b, 255, img[:, :, 1])
+        img[:, :, 2] = np.where(a, 255, img[:, :, 2])
 
         #img[:, :, 0] = np.where(white, 255, img[:, :, 0])
         #img[:, :, 1] = np.where(white, 255, img[:, :, 1])
