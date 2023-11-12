5ade0582899bb6f6129bffa82c86085037dd4a8e
RooHam <roohamcomp@gmail.com>
2023-11-12 16:14:48 +0900

    .

--- src/vision.py
+++ src/vision.py
@@ -59,7 +59,7 @@ class VisionImage(Submodule):
         y1 = self.get_yellow()&(~black)
         y1[0:124, 0:252] &= y1[4:128, 4:256]
         #y1[4:128, 4:256] |= y1[0:124, 0:252]
-        #y1[0:127, 0:255] &= ~y1[1:128, 1:256]
+        y1[0:126, 0:254] &= ~y1[2:128, 2:256]
         y1[0:8, :], y1[120:128, :], y1[:, 0:8], y1[:, 248:256] = False, False, False, False
 
         a = self.get_yellow_point(y1)
