206060974b41fa102ff040034cd8c89c20734a4e
RooHam <roohamcomp@gmail.com>
2023-11-12 17:39:00 +0900

    .

--- src/vision.py
+++ src/vision.py
@@ -58,9 +58,9 @@ class VisionImage(Submodule):
         black = self.get_white()
         white = self.get_black()
         bw = black | white
-        bw[:, 0:252] &= bw[:, 4:256]
+        bw[:, 0:254] &= bw[:, 2:256]
         y1 = self.get_yellow()&(~bw)
-        for n in range(4):
+        for n in range(2):
             y1[:, 0:255] &= y1[:, 1:256]
         y1[0:8, :], y1[120:128, :], y1[:, 0:8], y1[:, 248:256] = False, False, False, False
         y2 = np.zeros((128,256), bool)
@@ -70,13 +70,13 @@ class VisionImage(Submodule):
         points_tangent = []
         for x, y in points_coord:
             if y == 128 : continue
-            tan1 = (x-64)/(y-128)
+            tan1 = np.atan((x-64)/(y-128))
             mask = np.ones((17,17), np.int32)
             mask[1:16, 1:16] = 0
             base = y2[-8+x:9+x, -8+y:9+y].copy() * mask
             base_sum = np.sum(base)
             base_coord = np.array(np.where(base != 0)).T
-            com = sum([0 if j == 8 else (i-8)/(j-8) for i, j in base_coord])
+            com = sum([0 if j == 8 else np.atan((i-8)/(j-8)) for i, j in base_coord])
             if base_sum == 0 : continue
             tan2 = com/base_sum
             points_tangent.append((tan1, tan2))
