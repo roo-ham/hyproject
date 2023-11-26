9703ff54e5f9f35c0ee5033e63499d3cb87b5254
RooHam <roohamcomp@gmail.com>
2023-11-27 07:51:42 +0900

    파라미터 미세조정

--- src/storage.py
+++ src/storage.py
@@ -113,9 +113,9 @@ class Lane(Storage):
 
         gtan, ltan_abs = self.timescale_dataset[0, 0], self.timescale_dataset[0, 2]
 
-        # 커브를 발견하면 2.6m 타이머 시작
+        # 커브를 발견하면 2.2m 타이머 시작
         if self.found_junction(gtan) and self.timer <= 0:
-            self.pause_until(2.6)
+            self.pause_until(2.2)
         
         # 차선이 수평하면 (휘어있으면) 속도 줄임
         # 그렇지 않으면 (곧으면) 속도 늘림
@@ -125,7 +125,7 @@ class Lane(Storage):
         delta_z = gtan * 0.5
         
         # 급커브 처리
-        if self.timer > 0.9 :
+        if self.timer > 0.5 :
             delta_x += 1.0
             arc_offset = 0.4
             if (gtan > 0):
