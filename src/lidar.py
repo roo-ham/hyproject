74f451c34543e92e3e77e4fbda4ff882df23d6db
RooHam <roohamcomp@gmail.com>
2023-11-27 12:27:46 +0900

    .

--- src/lidar.py
+++ src/lidar.py
@@ -10,19 +10,16 @@ class Lidar(Submodule):
     def __init__(self, base:Basement):
         super().__init__(base, "Lidar")
         rospy.Subscriber('/scan', LaserScan, self.callback)
-        self.wall_storage:storage.Lane = base.storages["wall"]
-        self.orthogonal_pos = []
-        self.polar_pos = []
+        self.wall_storage:storage.Wall = base.storages["wall"]
         
     def callback(self, data):
         super().callback(data)
-        print(".")
-        self.orthogonal_pos = []
-        self.polar_pos = []
+        orthogonal_pos = []
+        polar_pos = []
         for n, radius in enumerate(data.ranges):
             if radius < data.range_min:
                 continue
             angle = data.angle_min + data.angle_increment * n
-            self.orthogonal_pos.append(np.array((radius, angle)))
-            self.polar_pos.append(radius * np.array((np.sin(angle), np.cos(angle))))
-        self.wall_storage.update(self.orthogonal_pos, self.polar_pos)
+            orthogonal_pos.append(np.array((radius, angle)))
+            polar_pos.append(radius * np.array((np.sin(angle), np.cos(angle))))
+        self.wall_storage.update(orthogonal_pos, polar_pos)
