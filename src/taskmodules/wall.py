61096ab038167901f9c6ccd16288866cccc2eaa1
RooHam <roohamcomp@gmail.com>
2023-11-30 22:09:20 +0900

    벽을 살짝 피하는 기능 추가

--- src/taskmodules/wall.py
+++ src/taskmodules/wall.py
@@ -16,25 +16,41 @@ class Wall(TaskModule):
         self.x = 0.0
         self.z = 0.0
 
-        in_range_pos = []
+        left_points = []
+        front_points = []
+        right_points = []
+
         for p1, p2 in zip(polar_pos, orthogonal_pos):
-            if p1[0] > 0.4 or abs(p1[1]) > 0.1:
-                continue
-            in_range_pos.append(p2)
-        if len(in_range_pos) < 5:
+            if p1[0] < 0.25 and p1[1] < -0.4:
+                left_points.append(p2)
+            if p1[0] < 0.4 and abs(p1[1]) < 0.1:
+                front_points.append(p2)
+            if p1[0] < 0.25 and p1[1] > 0.4:
+                right_points.append(p2)
+
+        if len(left_points) > 10:
+            self.weight_z = 0.2
+            self.z += -0.5
+
+        if len(right_points) > 10:
+            self.weight_z = 0.2
+            self.z += 0.5
+
+        if len(front_points) < 5:
             set_timer("wall/obstacle_ignore", 0.2, True)
             set_timer("wall/waiting_rotation", 5, True)
             return
-        elif is_timer_running("wall/obstacle_ignore"):
+        
+        if is_timer_running("wall/obstacle_ignore"):
             return
 
-        atan = get_wall_angle(in_range_pos)
+        atan = get_wall_angle(front_points)
 
         delta_x = 0.0
         delta_z = atan
         
         self.weight_x = 1000*atan
-        self.weight_z = 1
+        self.weight_z = 1.0
 
         if is_timer_running("wall/waiting_rotation"):
             self.weight_z = 0
