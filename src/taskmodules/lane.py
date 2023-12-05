09af0b97289cb928ca1f0b42e867715b38546166
RooHam <roohamcomp@gmail.com>
2023-12-05 19:33:59 +0900

    .

--- src/taskmodules/lane.py
+++ src/taskmodules/lane.py
@@ -106,10 +106,10 @@ class Lane(TaskModule):
             gtan = None
         if is_timer_running("lane/lane_exception/left"):
             set_timer("lane/lane_exception/left", -1, True)
-            gtan = -1.0
+            gtan = -np.pi/2
         elif is_timer_running("lane/lane_exception/right"):
             set_timer("lane/lane_exception/right", -1, True)
-            gtan = 1.0
+            gtan = np.pi/2
         if gtan == None and ltan == None:
             set_timer("lane/ramp", 0.4)
         self.append_latest_data(gtan, ltan, ltan_abs)
