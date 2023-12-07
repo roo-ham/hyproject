f99a74d801370f782bef7f5ff6c244f0cbf0e72f
RooHam <roohamcomp@gmail.com>
2023-12-07 22:08:09 +0900

    .

--- src/taskmodules/lane.py
+++ src/taskmodules/lane.py
@@ -116,7 +116,7 @@ class Lane(TaskModule):
             delta_x = 2.0
             self.clean_junction_curve()
         elif abs(delta_z) > 0.2 and abs(gtan) < 1.1:
-            delta_z = self.do_junction_curve((gtan * 0.5) - ltan)
+            delta_z = self.do_junction_curve((gtan * 0.8) - ltan)
         else:
             delta_x = 1.5
             delta_z = (gtan - (ltan*0.5))/1.25
