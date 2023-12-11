c67cd45cfaf9d9b3eafdd0c958c1cfbb90fadfe2
RooHam <roohamcomp@gmail.com>
2023-12-11 13:06:46 +0900

    2023년 12월 11일 첫 커밋

--- src/taskmodules/lane.py
+++ src/taskmodules/lane.py
@@ -127,9 +127,17 @@ class Lane(TaskModule):
             if is_none[1]:
                 delta_z = gtan/1.1
 
-        if is_timer_on("lane/ramp"):
+        if is_timer_off("lane/ramp"):
+            pass
+        elif (not is_none[2]) and ltan_abs < 0.3:
+            set_timer("lane/front_blocked", 1.5)
+        else:
             delta_x = 0.8
             delta_z = 0
+
+        if is_timer_on("lane/front_blocked"):
+            delta_x = 0.2
+            delta_z = -1.0
 
         if is_timer_on("lane/junction/wait"):
             pass
