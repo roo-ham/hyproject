68e2d57a763da17a47e96ac654d2326c1c553ea5
RooHam <roohamcomp@gmail.com>
2023-12-12 14:46:05 +0900

    .

--- src/taskmodules/lane.py
+++ src/taskmodules/lane.py
@@ -130,21 +130,15 @@ class Lane(TaskModule):
         else:
             delta_x = 1.3
             delta_z = 0.0
-            if is_none[1]:
+            if is_none[0]:
                 set_flag("lane/curve", True)
 
         if is_not_flag("lane/curve") and is_timer_on("lane/ramp"):
             delta_x = 0.8
             delta_z = 0
         elif is_flag("lane/curve"):
-            delta_x = 1.3
+            delta_x = 0.8
             delta_z = gtan/2
-
-        if is_timer_off("lane/ramp"):
-            pass
-        elif (not is_none[2]) and ltan_abs < 0.3 :
-            set_timer("lane/front_blocked/wait", 2.8)
-            set_timer("lane/front_blocked", 2.8 + 1.75)
 
         if is_timer_on("lane/front_blocked/wait"):
             pass
