936b5e368c9b57349bbf0dc10331b192b0ab0779
RooHam <roohamcomp@gmail.com>
2023-12-13 22:33:13 +0900

    .

--- src/taskmodules/lane.py
+++ src/taskmodules/lane.py
@@ -213,7 +213,7 @@ class Lane(TaskModule):
             self.weight_z = 0.0
             if 0.5 < yellow_distribution and ltan_abs < 0.2:
                 set_flag("tpark/approach/end", True)
-            if is_not_flag("tpark/approach/end") and abs(gtan) > 0.5:
+            if is_not_flag("tpark/approach/end") and 0.5 < abs(gtan) and abs(gtan) <= 1.0 :
                 self.weight_z = 1.0
                 delta_z = ltan/4
 
